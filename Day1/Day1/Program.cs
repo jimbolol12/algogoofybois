f2be0c3dfd0a549639abb35180a163bd1da4e997
jimbolol12 <97526093+jimbolol12@users.noreply.github.com>
2023-09-28 09:56:50 +0200

    day 1 complete

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,39 @@ namespace Day1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string inputFileName = "input.txt";
+
+            string input = File.ReadAllText(inputFileName);
+
+            List<string> lines = input.Split('\n').ToList();
+
+            List<int> elfCalories = new List<int>();
+            int currentElfCalories = 0;
+
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    elfCalories.Add(currentElfCalories);
+                    currentElfCalories = 0;
+                }
+                else
+                {
+                    currentElfCalories += int.Parse(line);
+                }
+            }
+            if (currentElfCalories > 0)
+            {
+                elfCalories.Add(currentElfCalories);
+            }
+
+            int maxCalories = elfCalories.Max();
+
+            Console.WriteLine($"De Elf met de meeste calorieën draagt {maxCalories} calorieën.");
+            Console.ReadLine();
         }
     }
 }
+
